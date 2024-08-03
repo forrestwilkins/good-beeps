69ddfcce9918504ec49180f4fd785d6cff24ff31
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-08-03 15:24:40 -0400

    feat: make noise for 5 seconds

--- main.go
+++ main.go
@@ -8,23 +8,23 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
-type Noise struct{}
-
-func (noise Noise) Stream(samples [][2]float64) (n int, ok bool) {
-	for i := range samples {
-		samples[i][0] = rand.Float64()*2 - 1
-		samples[i][1] = rand.Float64()*2 - 1
-	}
-	return len(samples), true
-}
-
-func (noise Noise) Err() error {
-	return nil
+func Noise() beep.Streamer {
+	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
+		for i := range samples {
+			samples[i][0] = rand.Float64()*2 - 1
+			samples[i][1] = rand.Float64()*2 - 1
+		}
+		return len(samples), true
+	})
 }
 
 func main() {
 	sr := beep.SampleRate(44100)
 	speaker.Init(sr, sr.N(time.Second/10))
-	speaker.Play(Noise{})
-	select {}
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(beep.Take(sr.N(5*time.Second), Noise()), beep.Callback(func() {
+		done <- true
+	})))
+	<-done
 }
